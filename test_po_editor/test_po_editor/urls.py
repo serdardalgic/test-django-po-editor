7c357dfb6bce5a9d991508a13536b4a7eb34e162
Serdar Dalgıç <sd@serdardalgic.org>
2016-03-24 18:05:32 +0100

    Add locale files.

--- test_po_editor/test_po_editor/urls.py
+++ test_po_editor/test_po_editor/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 from . import views
@@ -6,9 +7,9 @@ from . import views
 admin.autodiscover()
 
 
-urlpatterns = [
-    url(r'^app1/', include('app1.urls')),
-    url(r'^app2/', include('app2.urls')),
-    url(r'^admin', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
-]
+urlpatterns = i18n_patterns('',
+                            url(r'^app1/', include('app1.urls')),
+                            url(r'^app2/', include('app2.urls')),
+                            url(r'^admin', include(admin.site.urls)),
+                            url(r'^$', views.index, name='index'),
+                            )
