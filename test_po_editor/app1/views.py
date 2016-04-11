c3a69cc7933cd6520486993e6299ba5b28dc0058
Serdar Dalgıç <sd@serdardalgic.org>
2016-04-11 17:37:35 +0200

    Add new string to app1-change2 branch.

--- test_po_editor/app1/views.py
+++ test_po_editor/app1/views.py
@@ -9,6 +9,7 @@ month = pgettext("month name", "May")
 
 def index(request):
     return_str = _("Hello from app1<br>")
-    return_str = ("{}<br>").format(month)
+    return_str += _("A New String for app1: app1-change2")
+    return_str += ("{}<br>").format(month)
     return_str += _('<a href="{}">Back to main page</a>').format(reverse('index'))
     return HttpResponse(return_str)
