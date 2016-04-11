dfeaf72c9734b710cc1521fa8761ab68e56d41c1
Serdar Dalgıç <sd@serdardalgic.org>
2016-04-11 17:21:06 +0200

    Add a pgettest test.

--- test_po_editor/app1/views.py
+++ test_po_editor/app1/views.py
@@ -1,11 +1,14 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from django.utils.translation import ugettext as _
+from django.utils.translation import pgettext, ugettext as _
 
 # Create your views here.
+
+month = pgettext("month name", "May")
 
 
 def index(request):
     return_str = _("Hello from app1<br>")
+    return_str = ("{}<br>").format(month)
     return_str += _('<a href="{}">Back to main page</a>').format(reverse('index'))
     return HttpResponse(return_str)
